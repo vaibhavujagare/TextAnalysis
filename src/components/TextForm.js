2146c704adc2557573d7f2dc6ac4f02e7beccd6a
vaibhavujagare <vaibhavujagare77@gmail.com>
2022-11-27 13:12:12 +0530

    Update TextForm

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,15 +5,16 @@ export default function TextForm(props) {
     const handleUpClick= ()=>{
         console.log(text);
         let new_text=text.toUpperCase();
-        setText( new_text);
+        setText(new_text);
     }
     const handleOnChange= (event)=>{
-        console.log("onchanged");
+      
         setText(event.target.value);
     }
 
     const [text,setText]=useState();
     return (
+        <>
         <div className="container">
         <div className="mb-3">
             <h1>{props.heading} </h1>            
@@ -21,5 +22,14 @@ export default function TextForm(props) {
             <button className="btn btn-primary mt-3" onClick={handleUpClick}>Analyse</button>
         </div>
         </div>
+
+        <div className="container my-2">
+            <h1>Your Text Summary</h1>
+            <p>
+                your word count {text.split(' ').length-1} and character count {text.length}
+            </p>
+        </div>
+       
+        </>
     )
 }   
